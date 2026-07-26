d32cf23271e916851fcb2c36fb8560d6767e4f2e
sanjeev-ccts <ksanjeev.nit@gmail.com>
2026-07-26 14:10:55 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'thali-v1';
+const ASSETS = ['./health.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
