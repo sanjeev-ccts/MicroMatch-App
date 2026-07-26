8a1bae5924a054c3658a4f42cc911565a48b7d43
sanjeev-ccts <ksanjeev.nit@gmail.com>
2026-07-26 15:36:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE = 'thali-v1.2';
-const ASSETS = ['./health.html', './manifest.json'];
+const CACHE = 'thali-v2';
+const ASSETS = ['./health.html', './manifest.json', './my-icon.png', './login-bg.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
