a2dc3ed407bb5ed557621a129d7799bf3320f2f4
sanjeev-ccts <ksanjeev.nit@gmail.com>
2026-07-30 21:52:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'thali-v2.43';
+const CACHE = 'thali-v2.2';
 const ASSETS = ['./health.html', './manifest.json', './my-icon.png', './logo.png'];
 
 self.addEventListener('install', e => {
